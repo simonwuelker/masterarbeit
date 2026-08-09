545a1e6e187e2821204fff75dbcea168321ebdb3
Simon Wülker <simon.wuelker@arcor.de>
2026-08-10 00:11:23 +0200

    Fix run script
    Signed-off-by: Simon Wülker <simon.wuelker@arcor.de>

--- run.sh
+++ run.sh
@@ -1,15 +1,5 @@
-mkdir instances
-# wget "https://benchmark-database.de/getinstances?query=track%3Dmain_2025&context=cnf"   -O track_main_2025.uri
-# wget --content-disposition -i track_main_2025.uri -O instances/
-wget https://benchmark-database.de/file/0205e2dffaef93a90c239df31755f2e1?context=cnf -O instances/mihal.cnf.xz
-# xz --decompress instances/mihal.cnf.xz
+#!/bin/bash
 
-# Setup mallob
-git clone https://github.com/domschrei/mallob
-cd mallob
-git checkout fullcmake
-mkdir build
+spack env activate masterarbeit
 
-./scripts/server/create_mallob_env.sh
-./scripts/server/build_and_run_example.sh
-git apply patches/0001-Patch-mallob-for-server.patch
+cargo r -r -- server --mallob=../mallob --problem-directory=../mallob/problems --temp-directory=temp
