93533717070d7f0e040994997b2cc43f5f415dd6
Simon Wülker <simon.wuelker@arcor.de>
2026-08-09 15:26:38 +0200

    Allow combining online covariances
    Signed-off-by: Simon Wülker <simon.wuelker@arcor.de>

--- palrup/src/evaluation/online_covariance.rs
+++ palrup/src/evaluation/online_covariance.rs
@@ -1,3 +1,4 @@
+/// Computes covariance of data points in `O(1)` memory using welford.
 #[derive(Clone, Copy, Debug, Default)]
 pub(crate) struct OnlineCovariance {
     average_x: f64,
@@ -52,6 +53,33 @@ impl OnlineCovariance {
 
         Some(self.covariance / (self.m2_x * self.m2_y).sqrt())
     }
+
+    pub(crate) fn combine(first: OnlineCovariance, second: OnlineCovariance) -> OnlineCovariance {
+        let n1 = first.number_of_samples as f64;
+        let n2 = second.number_of_samples as f64;
+        let n = n1 + n2;
+
+        // Handle empty accumulators
+        if first.number_of_samples == 0 {
+            return second;
+        }
+        if second.number_of_samples == 0 {
+            return first;
+        }
+
+        let dx = second.average_x - first.average_x;
+        let dy = second.average_y - first.average_y;
+        let w = (n1 * n2) / n;
+
+        OnlineCovariance {
+            average_x: (n1 * first.average_x + n2 * second.average_x) / n,
+            average_y: (n1 * first.average_y + n2 * second.average_y) / n,
+            m2_x: first.m2_x + second.m2_x + dx * dx * w,
+            m2_y: first.m2_y + second.m2_y + dy * dy * w,
+            covariance: first.covariance + second.covariance + dx * dy * w,
+            number_of_samples: first.number_of_samples + second.number_of_samples,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -82,5 +110,24 @@ mod tests {
 
         let expected = -0.327327;
         assert!(covariance.pearson_correlation_coefficient().unwrap() - expected < 0.001);
+    }
+
+    #[test]
+    fn test_covariance_combine() {
+        let mut first_covariance = OnlineCovariance::default();
+
+        for (x, y) in iter::zip(&[1., 2., 4.], &[6., 0., 3.]) {
+            first_covariance.add_sample(*x, *y);
+        }
+
+        let mut second_covariance = OnlineCovariance::default();
+        for (x, y) in iter::zip(&[-1., 3., 0.], &[10., 1., 8.]) {
+            second_covariance.add_sample(*x, *y);
+        }
+
+        let combined = OnlineCovariance::combine(first_covariance, second_covariance);
+
+        assert_eq!(combined.population_covariance(), Some(-5.166666666666667));
+        assert_eq!(combined.sample_covariance(), Some(-6.2));
     }
 }
